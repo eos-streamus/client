b13e1ec6d6564f69f5c86b017468663755f58738
AkessonHenrik <henrik.akesson@outlook.com>
2020-07-25 13:40:51 +0200

    Make submit() login to server

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'auth',
@@ -9,14 +12,31 @@ export class AuthComponent implements OnInit {
   @ViewChild("email") private email: ElementRef;
   @ViewChild("password") private password: ElementRef;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   submit() {
-    console.log(this.email.nativeElement.value);
-    console.log(this.password.nativeElement.value);
+    this.authService.performLogin(
+      this.email.nativeElement.value,
+      this.password.nativeElement.value
+    )
+    .pipe(catchError(this.handleError<string>('login', '')))
+    .subscribe(token => console.log(token))
   }
 
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
