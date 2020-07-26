8b7dabcade3ca91be358f6dcc4f4ea1e7198fe2a
AkessonHenrik <henrik.akesson@outlook.com>
2020-07-26 22:21:57 +0200

    Create RegisterResponseSuccessData

--- src/app/responses/RegisterResponse.ts
+++ src/app/responses/RegisterResponse.ts
@@ -1,13 +1,28 @@
+export interface RegisterResponseSuccessData {
+  id: number,
+  firstName: string,
+  lastName: string,
+  dateOfBirth: number,
+  createdAt: string,
+  updatedAt: string,
+  email: string,
+  username: string
+}
+
 
 export class RegisterResponse {
   private _success: boolean;
   private _errors: [{ fieldName: string, error: string }];
+  private _data?: RegisterResponseSuccessData
+
 
   get success(): boolean { return this._success; }
   get errors(): [{ fieldName: string, error: string }] { return this._errors; }
+  get data(): RegisterResponseSuccessData { return this._data; }
 
-  constructor(success: boolean, errors: [{ fieldName: string, error: string }]) {
+  constructor(success: boolean, errors: [{ fieldName: string, error: string }], data?: RegisterResponseSuccessData) {
     this._success = success;
     this._errors = errors;
+    this._data = data;
   }
 }
