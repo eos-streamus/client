c11fae42f8bcfbee4b433b8de0d4af60fc619a2f
AkessonHenrik <henrik.akesson@outlook.com>
2020-07-26 22:48:54 +0200

    Create AuthGuard, which checks for jwtToken

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Constants } from './constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (localStorage.getItem('streamusToken')) {
+      return true;
+    }
+    return this.router.parseUrl('auth');
+  }
+
+}
