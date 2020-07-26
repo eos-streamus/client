ab4e5ca56cfd3e0f71ba81b7e83f198d69f97e03
AkessonHenrik <henrik.akesson@outlook.com>
2020-07-26 22:49:17 +0200

    Add routes for AuthComponent and CatalogComponent, protected by AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { AuthGuard } from './auth.guard';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CatalogComponent, canActivate: [AuthGuard] },
+  { path: 'auth', component: AuthComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
