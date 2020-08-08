874d8890355a2f989901b2fe499440a7763918fe
AkessonHenrik <henrik.akesson@outlook.com>
2020-08-09 01:31:31 +0200

    Only show search box and logout button if logged in, redirect to auth if logged out

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { LogoutService } from '../logout.service';
+import { AuthService } from '../auth.service';
+import { faSignOutAlt, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { JwtService } from '../jwt.service';
 
 @Component({
   selector: 'navbar',
@@ -6,7 +11,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  faSignOutAlt: IconDefinition = faSignOutAlt;
 
-  constructor() { }
+  constructor(public jwtService: JwtService, private logoutService: LogoutService, private router: Router) { }
 
+  isLoggedIn(): boolean {
+    return this.jwtService.getTokens() && this.jwtService.getTokens().sessionToken.expiresAt > Date.now();
+  }
+
+  logout() {
+    this.logoutService.logout().then(_ => {
+      this.router.navigateByUrl('/auth');
+    });
+  }
 }
