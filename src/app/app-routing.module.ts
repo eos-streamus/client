a813c6c530dbf4c839a5ab3b2866f2aa691dd0de
AkessonHenrik <henrik.akesson@outlook.com>
2020-09-13 19:31:05 +0200

    Add route to admin dashboard, protected by AdminGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { FilmCatalogComponent } from './film-catalog/film-catalog.component';
 import { AuthGuard } from './auth.guard';
+import { AdminGuard } from './admin.guard';
 import { VideoStreamComponent } from './video-stream/video-stream.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   { path: '', component: FilmCatalogComponent, canActivate: [AuthGuard] },
   { path: 'auth', component: AuthComponent },
-  { path: 'watchVideo/:id', component: VideoStreamComponent, canActivate: [AuthGuard] }
+  { path: 'watchVideo/:id', component: VideoStreamComponent, canActivate: [AuthGuard] },
+  { path: 'admin', component: AdminDashboardComponent, canActivate: [AdminGuard]}
 ];
 
 @NgModule({
