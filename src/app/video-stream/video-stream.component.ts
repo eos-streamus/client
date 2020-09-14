5894ddd006ef6d4dbd4ed3e59d1c7cd4621b41ba
AkessonHenrik <henrik.akesson@outlook.com>
2020-09-14 21:15:25 +0200

    Notify server when paused or window closed

--- src/app/video-stream/video-stream.component.ts
+++ src/app/video-stream/video-stream.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angula
 import { ActivatedRoute } from '@angular/router';
 import { Constants } from '../constants';
 import { AuthService } from '../auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-video-stream',
@@ -10,10 +11,12 @@ import { AuthService } from '../auth.service';
 })
 export class VideoStreamComponent implements OnInit, AfterViewInit {
   id: number;
+  activityId: number;
   @ViewChild('player') player: ElementRef;
   ready: boolean = false;
+  initialOffset: number = 0;
 
-  constructor(private route: ActivatedRoute, private authService: AuthService) { }
+  constructor(private route: ActivatedRoute, private httpClient: HttpClient) { }
 
   ngOnInit(): void {
     this.route.params.forEach(param => {
@@ -24,25 +27,23 @@ export class VideoStreamComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    ///activity/{resourceId}
-    if (this.player) {
-      this.player.nativeElement.onplay = this.onPlay;
-      this.player.nativeElement.onpause = this.onPause;
-      this.player.nativeElement.ontimeupdate = this.onTimeUpdate;
-    }
+    this.httpClient.get<{ id: number, pausedAt: number }>(Constants.getUrl(`activity/${this.id}`)).toPromise().then(res => {
+      this.initialOffset = res.pausedAt;
+      this.activityId = res.id;
+      this.ready = true;
+      if (this.player) {
+        this.player.nativeElement.currentTime = this.initialOffset;
+        const self = this;
+        this.player.nativeElement.onpause = () => self.notifyUpdatedPausedAt();
+        window.addEventListener(`beforeunload`, () => self.notifyUpdatedPausedAt());
+      }
+    });
   }
 
-  private onTimeUpdate(event) {
-    console.log("Time updated");
-    console.log(event);
-  }
-
-  private onPlay() {
-    console.log("Play");
-  }
-
-  private onPause() {
-    console.log("Pause");
+  private notifyUpdatedPausedAt(): void {
+    this.httpClient
+      .post<{ id: number, pausedAt: number }>(Constants.getUrl(`activity/${this.activityId}/pause/${Math.floor(this.player.nativeElement.currentTime)}`), null)
+      .subscribe()
   }
 
   public getUrl(): string {
